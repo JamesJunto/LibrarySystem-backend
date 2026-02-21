c11e7d28610c979dccea029a07b5de7be0830c70
Chamiruuuu <jamesjunto22@gmail.com>
2026-02-21 16:46:21 +0800

    update status

--- backend/addBorrowBook.php
+++ backend/addBorrowBook.php
@@ -5,16 +5,47 @@ require_once 'connection.php';
 
 $data = json_decode(file_get_contents('php://input'), true);
 $bookId = $data['book_id'] ?? null;
-
 $user_id = $_SESSION['user_id'] ?? null;
 
-$sql = "INSERT INTO borrowed (user_id, book_id) VALUES (?, ?)";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ii", $user_id, $bookId);
-
-if ($stmt->execute()) {
-    echo json_encode(["status" => "success", "message" => "Book added successfully"]);
-} else {
-    echo json_encode(["status" => "error", "message" => "Failed to add book"]);
+if (!$bookId || !$user_id) {
+    echo json_encode(["status" => "error", "message" => "Invalid data"]);
+    exit;
 }
 
+$conn->begin_transaction();
+
+try {
+
+    // INSERT BOOKS ON BORROWED
+    $sql1 = "INSERT INTO borrowed (user_id, book_id) VALUES (?, ?)";
+    $stmt1 = $conn->prepare($sql1);
+    $stmt1->bind_param("ii", $user_id, $bookId);
+    $stmt1->execute();
+
+    // UPDATE books
+    $sql2 = "UPDATE books SET status = 1 WHERE book_id = ?";
+    $stmt2 = $conn->prepare($sql2);
+    $stmt2->bind_param("i", $bookId);
+    $stmt2->execute();
+
+
+
+    $conn->commit();
+
+    echo json_encode([
+        "status" => "success",
+        "message" => "Book borrowed successfully"
+    ]);
+
+} catch (Exception $e) {
+
+    $conn->rollback();
+
+    echo json_encode([
+        "status" => "error",
+        "message" => "Borrow failed"
+    ]);
+}
+
+$conn->close();
+?>
--- backend/getBook.php
+++ backend/getBook.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 require_once "cors.php";
 require_once "connection.php";
 
